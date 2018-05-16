52b74f4d598a7352debf80e61463f9288e46919d
selin <denizgungor10@gmail.com>
2018-05-16 22:48:05 +0200

    Bar graph is created based on neg, pos, neutral

---
+++ swe573/analyze/graphstuff/Graph.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Graph(object):
+    def __init__(self, analyse_results):
+        self.analyse_results = analyse_results
+
+    def draw_graph(self):
+        neg = []
+        neu = []
+        pos = []
+        comp = []
+        x = []
+        y = []
+
+        for result in self.analyse_results:
+            print(result)
+            print(type(result))
+            r = list(result)
+            print(list(result))
+            neg.append(str(r[0])[8:-1])
+            neu.append(str(r[1])[8:-1])
+            pos.append(str(r[2])[8:-1])
+            comp.append(str(r[3])[13:-1])
+            print(str(r[3])[13:-1])
+            print("-------------------------------------------------------------------------"
+                  ""
+                  ""
+                  ""
+                  ""
+                  "---------------------------------------------------------------------")
+
+        neg = self.string_to_float(neg)
+        neu = self.string_to_float(neu)
+        pos = self.string_to_float(pos)
+        # comp = self.string_to_float(comp)
+
+        # self.negative = self.column(self.analyse_results, 0)
+        # self.nautral = self.column(self.analyse_results, 1)
+        # self.positive = self.column(self.analyse_results, 2)
+        #
+        fig = plt.figure()
+        plt.title('Sentiment Analysis')
+        plt.xlabel('tweet sayisi burasi sanirim guzel bir isim vermek lazim')
+        plt.ylabel('Values')
+        print(type(neg))
+        print(neg)
+
+        x = range(len(neg))
+        # plot the data itself
+        #plt.plot(x, neg, 'o')
+        #plt.plot(x, neu, 'o')
+        #plt.plot(x, pos, 'o')
+        # plt.plot(x, neg, 'o', c=neg)
+        # plt.plot(x, neu, 'o', c=neg)
+        # plt.plot(x, pos, 'o', c=neg)
+        # plt.plot(x, neg,'o')
+
+        # # calc the trendline (it is simply a linear fitting)
+        # z = numpy.polyfit(x, y, 1)
+        # p = numpy.poly1d(z)
+        # plt.plot(x, p(x),"r--")
+        # the line equation:
+
+        #colors = [(1-n, 1-n, 1-n) for n in neg]
+
+        stacked_neu = neg
+        stacked_pos = neg + neu
+
+        plt.bar(range(len(neg)), neg, align="center", color='coral')
+        plt.bar(range(len(neg)), neu, bottom=stacked_neu, align="center", color='khaki')
+        plt.bar(range(len(neg)), pos, bottom=stacked_pos, align="center", color='forestgreen')
+        fig.savefig('denizgraph.png', dpi=fig.dpi)
+
+
+        # plt.bar(range(len(neg)), neg, align="center")
+        # plt.xticks(range(len(neg)), neg)
+        # fig.savefig('denizgraph.png', dpi=fig.dpi)
+        # fig.savefig(settings.BASE_DIR + settings.STATIC_URL + '/denizgraph.png')
+        return plt
+
+
+    def string_to_float(self, my_list):
+        return np.array([float(i) for i in my_list])
+
+
--- swe573/analyze/views.py
+++ swe573/analyze/views.py
@@ -12,55 +12,74 @@ from bs4 import BeautifulSoup
 from textblob import TextBlob
 import operator
 import preprocessor as p
+from analyze.graphstuff.Graph import Graph
 
 consumer_key = 'bT3x6y2rZCI6h6PQlaAOeQlAu'
 consumer_secret = 'MpcpsCdVfixFSbbQnnHvr5fxgZmx2Dx2fQCwzppfPDOMvbphZh'
 access_token = '995715271208919040-kJlqUCQuV14dSpflm6BqRxFOlwBT987'
 access_token_secret = 'o6CQaDFtiVRgCuTWNCwc4qLfSOvkAJfXM4RCL0g7KzeWV'
 var1 = "data123"
+
+# global variables
+global_sentiment_analysis = ""
+nltk.download('vader_lexicon')
+
 # Create your views here.
 @login_required
 def home(request):
     return render(request, 'analyze/home.html')
 
+
 @login_required
 def test(request, string):
-	resultDataSet = list()
-	sentimentDataSet = []
-	sentimentValueDataSet = []
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
-	api = tweepy.API(auth)
-	searchquery = string+'-filter:retweets'
-	requestData = api.search(q=searchquery, count=15, lang='en')
-	jsonObject = json.dumps([status._json for status in requestData])
-	results = json.loads(jsonObject)
-	for tweet in results:
-		p.set_options(p.OPT.URL, p.OPT.MENTION, p.OPT.RESERVED)
-		tweetData = p.clean(tweet['text'])
-		#print(tweetData)
-		#sid = SentimentIntensityAnalyzer()
-		#ss = sid.polarity_scores(tweetData)
-		#text = str([text.encode('utf-8') for text in tweet])
-		#sid = SentimentIntensityAnalyzer()
-		#ss = sid.polarity_scores("a")
-		#testimonial = TextBlob(text)
-		resultDataSet.append(tweet)
-	
-	for i in resultDataSet:
-		sid = SentimentIntensityAnalyzer()
-		ss = sid.polarity_scores(i['text'])
-		#result_sentiment = max(ss.items(), key=operator.itemgetter(1))[0]
-		result_sentiment = ss.items()
-		sentimentDataSet.append(result_sentiment)
-    
-	return render(request, 'analyze/test.html', {'data': resultDataSet, 'sentiments': sentimentDataSet, 'string':string, 'scoreList': ss.items()})
+    resultDataSet = list()
+    sentimentDataSet = []
+    sentimentValueDataSet = []
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    searchquery = string + '-filter:retweets'
+    requestData = api.search(q=searchquery, count=15, lang='en')
+    jsonObject = json.dumps([status._json for status in requestData])
+    results = json.loads(jsonObject)
+    for tweet in results:
+        p.set_options(p.OPT.URL, p.OPT.MENTION, p.OPT.RESERVED)
+        tweetData = p.clean(tweet['text'])
+        # print(tweetData)
+        # sid = SentimentIntensityAnalyzer()
+        # ss = sid.polarity_scores(tweetData)
+        # text = str([text.encode('utf-8') for text in tweet])
+        # sid = SentimentIntensityAnalyzer()
+        # ss = sid.polarity_scores("a")
+        # testimonial = TextBlob(text)
+        resultDataSet.append(tweet)
+
+    for i in resultDataSet:
+        sid = SentimentIntensityAnalyzer()
+        ss = sid.polarity_scores(i['text'])
+        # result_sentiment = max(ss.items(), key=operator.itemgetter(1))[0]
+        result_sentiment = ss.items()
+        sentimentDataSet.append(result_sentiment)
+
+    global_sentiment_analysis = get_sentiment_graph(sentimentDataSet)
+    return render(request, 'analyze/test.html',
+                  {'data': resultDataSet, 'sentiments': sentimentDataSet, 'string': string, 'scoreList': ss.items()})
+
+
+def get_sentiment_graph(data_set):
+    result = Graph(data_set).draw_graph()
+    print("--------------------------------Denizdenizdeniz------------------------")
+    print(result)
+    return result
+
 
 def logout(request):
     return render(request, 'analyze/logout.html', {})
 
+
 def login(request):
     return render(request, 'analyze/login.html', {})
+
 
 def register(request):
     if request.method == 'POST':
@@ -75,7 +94,5 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'analyze/register.html', {'form': form})
-
-
 
 #			resultDataSet.append(k+"-_-"+ss[k]+"-_-"+tweet)
